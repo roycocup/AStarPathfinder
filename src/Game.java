8727fc59baf25914c0c98bf624dfceef550c93d8
Rodrigo Dias <rodrigo.pitta@gmail.com>
2017-07-06 17:49:16 +0100

    more

--- src/Game.java
+++ src/Game.java
@@ -1,17 +1,17 @@
 
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Vector;
 
-import javafx.util.Pair;
 import processing.core.PApplet;
 
 public class Game extends PApplet {
 
+	Node start; 
+	Node goal; 
+	
 	HashMap<int[], Node> nodes;
-	Vector<Node> openSet;
-	Vector<Node> closedSet;
+	Vector<Node> openSet = new Vector<Node>();
+	Vector<Node> closedSet = new Vector<Node>();
 
 
 	int numRows;
@@ -23,17 +23,111 @@ public class Game extends PApplet {
 	}
 
 	public void setup(){
-
-
 		numRows = height / w;
 		numCols = width / w;
-
+		
+		goal = new Node(this, numCols, numRows);
+		start = new Node(this, 0, 0);
+		
 		createNodes();
+		openSet.add(start);
 	}
 
 	public void draw(){
 		background(0);
 		drawGrid();
+		run();
+	}
+	
+	int getLowestF(){
+		
+		float bestF = Float.POSITIVE_INFINITY;
+		int selectedIndex = -1;
+		for(int i = 0; i < openSet.size(); i++){
+			
+			if (openSet.get(i).f < bestF){
+				bestF = openSet.get(i).f;
+				selectedIndex = i;
+			}
+			
+		}
+		return selectedIndex;
+	}
+	
+	Boolean isOpenSetEmpty(){
+		if (openSet.size() < 1){
+			return true;
+		}
+		return false;
+	}
+	
+	void paintSquares(){
+		for(Node n : openSet){
+        	fill(0,255,0);
+        	rect(n.pos.x, n.pos.y, w, w);
+        }
+        for(Node n : closedSet){
+        	fill(255,0,0);
+        	rect(n.pos.x, n.pos.y, w, w);
+        }
+	}
+	
+	Boolean isInOpenSet(){
+		return false;
+	}
+	
+	Boolean isInClosedSet(Node n){
+		if(closedSet.contains(n))
+			return true;
+		return false;
+	}
+	
+	Boolean run(){
+		if (isOpenSetEmpty())
+			return false;
+		
+		int index = getLowestF();
+		Node current = openSet.get(index);
+		
+		if (current.equals(goal)){
+			println("finished!");
+			// should reconstruct and return the path sequence from all the cameFroms
+			return false;
+		}
+		
+		openSet.remove(index);
+        closedSet.add(current);
+
+        paintSquares();
+        
+        
+        for(int i = 0; i < current.neighbours.size(); i++){
+        	if (isInClosedSet(new Node(this, (int) current.neighbours.get(i).x, (int) current.neighbours.get(i).y))){
+        		continue;
+        	}
+        }
+        
+        /**
+        for each neighbor of current
+	        if neighbor in closedSet
+	            continue		// Ignore the neighbor which is already evaluated.
+	
+	        if neighbor not in openSet	// Discover a new node
+	            openSet.Add(neighbor)
+	        
+	        // The distance from start to a neighbor
+	        tentative_gScore := gScore[current] + dist_between(current, neighbor)
+	        if tentative_gScore >= gScore[neighbor]
+	            continue		// This is not a better path.
+	
+	        // This path is the best until now. Record it!
+	        cameFrom[neighbor] := current
+	        gScore[neighbor] := tentative_gScore
+	        fScore[neighbor] := gScore[neighbor] + heuristic_cost_estimate(neighbor, goal)
+         **/
+        
+        return false;
+ 
 	}
 
 	void createNodes(){
@@ -41,9 +135,7 @@ public class Game extends PApplet {
 
 		for(int i=0; i < numRows; i++ ){
 			for(int j=0; j < numCols; j++){
-				Node n = new Node(this);
-				n.col = j;
-				n.row = i;
+				Node n = new Node(this, j, i);
 				nodes.put(new int[]{j,i}, n);
 			}
 		}
@@ -52,8 +144,9 @@ public class Game extends PApplet {
 	void drawGrid(){
 		stroke(255);
 		for(int i=0; i < numRows; i++ ){
+			line(i*w, 0, i*w, height);
 			for(int j=0; j < numCols; j++){
-				line(i*w, j*w, w, w);
+				line(0, j*w, width, j*w);
 			}
 		}
 	}
--- src/Node.java
+++ src/Node.java
@@ -1,15 +1,70 @@
+import java.util.ArrayList;
+
+import processing.core.PVector;
 
 public class Node {
 	Game gr;
 	int col, row; 
+	PVector pos; 
+	Node cameFrom;
+	ArrayList<PVector> neighbours; 
 	
 	
-	float f;
-	float g; 
-	float h; 
+	// the cost of getting from the start node to this node.
+	float g = Float.POSITIVE_INFINITY; 
+	// heuristic cost
+	float h = Float.POSITIVE_INFINITY; 
+	// cost from start to this node + heuristic
+	float f = Float.POSITIVE_INFINITY;
 	
-	public Node(Game gr){
+	public Node(Game gr, int col, int row){
 		this.gr = gr;
+		this.col = col; 
+		this.row = row;
+		this.pos = new PVector(this.col,this.row);
+		setG();
+		setH();
+		setF();
+		findNeighbours();
+	}
+	
+	void setG(){
+		if (cameFrom == null){
+			g = 0;
+		} else {
+			g = 1;
+		}
+	}
+	
+	void setF(){
+		f = g + h; 
+	}
+	
+	void setH(){
+		if (gr.goal != null){
+			h = pos.dist(gr.goal.pos);
+		} else {
+			h = 0;
+		}
+		
+		
+	}
+	
+	void findNeighbours(){
+		neighbours = new ArrayList<PVector>();
+		
+		for(int i = -1; i <= 1; i++){
+			for(int j = -1; j <= 1; j++){
+				if(col == 0) if (i == -1) continue;
+				if(row == 0) if (j == -1) continue;
+				if(col == gr.numCols) if (i == 1) continue;
+				if(row == gr.numRows) if (j == 1) continue;
+				
+				neighbours.add(new PVector(i,j));
+			}
+		}
+		
+		
 	}
 	
 	public void update(){
